e7f0ed1db6c860f3b6c498935d6c767d84f43784
John O'Connell <johnoconnell@Johns-MacBook-Pro.local>
2017-08-04 19:30:36 -0500

    more of the read product function is done

--- bamazon.js
+++ bamazon.js
@@ -5,24 +5,24 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
 
-  // Your username
   user: "root",
 
-  // Your password
   password: "root",
   database: "bamazonDB"
 });
 
+//the 
 connection.connect(function(err){ 
-	readProducts();
+	
 	orderProducts();
+	readProducts();
 
 }); 
 
 
 
 function readProducts() {
-  console.log("Selecting all products...\n");
+  console.log("Next you can choose the quantity.");
   connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
@@ -31,6 +31,70 @@ function readProducts() {
         }
   	});
 }
+
+function orderProducts() { 
+inquirer.prompt ([
+	{
+		type: "input",
+		name: "itemID",
+		message: "What is the ID of the item that you would like to purchase?"
+	},
+	{
+		type: "input",
+		name: "quantity",
+		message: "How many units would you like to buy?"
+	}
+	]).then(function(response) {
+
+		var realID = response.id; 
+		var id = parseInt(realID) -1; 
+		var amount = parseInt(response.quantity); 
+
+		connection.query("SELECT * FROM products", function(err, result) {
+
+			var cost = result[id].price;
+			var inStock = result[id].stock_qty;
+			var stockUpdate = inStock - amount;
+
+			//check if item is in stock
+			if(inStock === 0 && amount > inStock) {
+				console.log("out of stock!")
+				//without being able to fulfill an order, placeOrder will run again.
+				orderProducts();
+
+			}
+			//check if there is sufficient quantity of item for requested amount
+		    else if(amount > inStock) {
+				console.log("insufficient quantity");
+				//without being able to fulfill an order, placeOrder will run again.
+				orderProducts();
+			} 
+			else {
+				//calculate the total of the bill
+				var total = amount * cost
+
+				//update the database
+				connection.query("UPDATE products SET ? WHERE ?", [
+					{
+						stock_qty: stockUpdate
+					}, {
+						item_id: trueID
+					}
+				]);
+
+				//tell customer their total.
+				console.log("Your purchase total is:  $" + total);
+
+				//end the connection.
+				connection.end();
+			}
+		});
+
+	});
+
+}
+
+
 
 
 
